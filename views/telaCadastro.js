a5ee7aa05ef2ffbc51322f0f24ef55d2bd8445ca
ManoellOliver <manoeloliver081@gmail.com>
2023-11-22 01:45:12 -0300

    Atualizando o front do crud

---
+++ views/telaCadastro.js
@@ -0,0 +1,19 @@
+// JavaScript - telaCadastro.js
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registration-form");
+    const successMessage = document.getElementById("success-message");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        // Restante do código...
+
+        // Simulação de sucesso de cadastro
+        successMessage.style.display = "block";
+        form.reset();
+    });
+});
